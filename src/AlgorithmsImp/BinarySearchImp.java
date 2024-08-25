3d2088c3122c8d923a53a8253d80fdb18fba4497
aravindsamy-git <samyaravind53@gmail.com>
2024-08-25 02:04:37 -0700

    Ordered Agnostic binary search

--- src/AlgorithmsImp/BinarySearchImp.java
+++ src/AlgorithmsImp/BinarySearchImp.java
@@ -3,9 +3,9 @@ package AlgorithmsImp;
 public class BinarySearchImp {
 
     public static void main(String[] args) {
-        int[] arr = {2,4,6,9,10,22,34,45,56,73};
+        int[] arr = {2, 4, 6, 9, 10, 22, 34, 45, 56, 73};
         int target = 56;
-        int index = binarySearch(arr,target);
+        int index = binarySearch(arr, target);
         System.out.println(index);
     }
 
@@ -13,15 +13,27 @@ public class BinarySearchImp {
         int start = 0;
         int end = arr.length - 1;
 
+        boolean isAsc = arr[start] < arr[end];
+
         while (start <= end) {
-            int mid = (start + end) / 2;
+            int mid = start + (end - start) / 2;
 
             if (arr[mid] == target) {
                 return mid;
-            } else if (arr[mid] < target) {
-                start = mid + 1;
+            }
+
+            if (isAsc) {
+                if (arr[mid] < target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             } else {
-                end = mid - 1;
+                if (arr[mid] > target) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
             }
         }
 
