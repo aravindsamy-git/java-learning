a2ec6ba62b49696a87aff2764e3cba366c14c78f
aravindsamy-git <samyaravind53@gmail.com>
2024-08-29 09:07:09 -0700

    fibonacci code updated

--- src/recursionImp/FibonacciProblem.java
+++ src/recursionImp/FibonacciProblem.java
@@ -3,13 +3,23 @@ package recursionImp;
 public class FibonacciProblem {
 
     public static void main(String[] args) {
-        System.out.println(fibo(50));
+        int n = 5;
+        System.out.println("Fibonacci number using Binet's formula for n=" + n + ": " + fibonacci(n));
     }
 
-    static long fibo(int n){
-        if(n < 2){
+    static long fibo(int n) {
+        if (n < 2) {
             return n;
         }
         return fibo(n - 1) + fibo(n - 2);
+    }
+
+    public static long fibonacci(int n) {
+        final double PHI = (1 + Math.sqrt(5)) / 2;
+        final double PSI = (1 - Math.sqrt(5)) / 2;
+
+        double fib = (Math.pow(PHI, n) - Math.pow(PSI, n)) / Math.sqrt(5);
+
+        return (long) Math.round(fib);
     }
 }
