95138f1017d97d4bdb15007c7d5981f7128ef461
aravindsamy-git <samyaravind53@gmail.com>
2024-08-30 23:48:53 -0700

    added findunique in array  using bitwise operators

---
+++ src/BItwiseOperator/FindUnique.java
@@ -0,0 +1,20 @@
+package BItwiseOperator;
+
+public class FindUnique {
+
+    public static void main(String[] args) {
+        int[] arr = {2,3,3,4,2,6,4};
+        System.out.println(findone(arr));
+    }
+
+    static int findone(int[] arr){
+        int unique = 0;
+
+        for(int n: arr){
+            unique ^= n;
+        }
+
+        return unique;
+    }
+
+}
