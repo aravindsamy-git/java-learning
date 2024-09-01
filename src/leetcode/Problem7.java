6d6eac3a015cc8191d9557b6d8abdb1c65126179
aravindsamy-git <samyaravind53@gmail.com>
2024-09-01 09:10:21 -0700

    added leetcode problem7

---
+++ src/leetcode/Problem7.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class Problem7 {
+    public int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            // Check for overflow before actually multiplying and adding
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0; // Overflow for positive numbers
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0; // Overflow for negative numbers
+            }
+
+            reversed = reversed * 10 + digit;
+        }
+
+        return reversed;
+    }
+}
