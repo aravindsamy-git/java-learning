b8bb25d891b87f398a686cbf55979c93089326a1
aravindsamy-git <samyaravind53@gmail.com>
2024-09-03 19:45:57 +0530

    added n to 1 using recursion

--- src/recursionImp/Nto1.java
+++ src/recursionImp/Nto1.java
@@ -3,14 +3,16 @@ package recursionImp;
 public class Nto1 {
     public static void main(String[] args) {
         nto1(5);
+        reverseNto1();
     }
 
     static void nto1(int n){
-        if(n == 5){
+        if(n == 0){
             return;
         }
 
         System.out.println(n);
         nto1(n-1);
     }
+
 }
