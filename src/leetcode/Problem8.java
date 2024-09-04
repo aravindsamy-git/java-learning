e48e3129ec16235d692d6240e2bc37df49088017
aravindsamy-git <samyaravind53@gmail.com>
2024-09-04 23:08:30 +0530

    added leetcode problem 8

---
+++ src/leetcode/Problem8.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+public class Problem8 {
+    public static void main(String[] args) {
+        System.out.println(myAtoi("42"));           // Output: 42
+        System.out.println(myAtoi("   -042"));      // Output: -42
+        System.out.println(myAtoi("1337c0d3"));     // Output: 1337
+        System.out.println(myAtoi("0-1"));          // Output: 0
+        System.out.println(myAtoi("words and 987"));// Output: 0
+    }
+
+    public static int myAtoi(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        int i = 0;
+        int n = s.length();
+        int sign = 1;
+        long result = 0;
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i < n) {
+            if (s.charAt(i) == '-') {
+                sign = -1;
+                i++;
+            } else if (s.charAt(i) == '+') {
+                i++;
+            }
+        }
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i) - '0');
+            if (result * sign > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (result * sign < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            i++;
+        }
+
+        return (int) (result * sign);
+    }
+}
