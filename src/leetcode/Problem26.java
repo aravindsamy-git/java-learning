570f3fe70f4c38c3836d201f3a0b537a70addd92
aravindsamy-git <samyaravind53@gmail.com>
2024-09-05 21:39:27 +0530

    added leetcode problem 26

--- src/leetcode/Problem26.java
+++ src/leetcode/Problem26.java
@@ -6,15 +6,4 @@ public class Problem26 {
         System.out.println(removeDuplicates(arr));
     }
 
-    public static int removeDuplicates(int[] nums) {
-        if (nums.length == 0) return 0;
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
-            if (nums[j] != nums[i]) {
-                i++;
-                nums[i] = nums[j];
-            }
-        }
-        return i + 1;
-    }
 }
