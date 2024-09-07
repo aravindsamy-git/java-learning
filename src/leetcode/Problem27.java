0d8531bf9ef0d67c3ca2afad820c1ddcacdaba2d
aravindsamy-git <samyaravind53@gmail.com>
2024-09-07 21:06:19 +0530

    added leetcode problem 27

--- src/leetcode/Problem27.java
+++ src/leetcode/Problem27.java
@@ -1,7 +1,10 @@
 package leetcode;
 
 public class Problem27 {
-
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,2,3,0,4,2};
+        System.out.println(removeElement(arr,2));
+    }
 
     public static int removeElement(int[] nums, int val) {
         int i = 0;
