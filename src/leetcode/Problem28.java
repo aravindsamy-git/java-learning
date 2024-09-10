e4bee3d806e184497116cf2e1139f8164d369968
aravindsamy-git <samyaravind53@gmail.com>
2024-09-10 22:20:49 +0530

    added leetcode problem28

--- src/leetcode/Problem28.java
+++ src/leetcode/Problem28.java
@@ -1,6 +1,11 @@
 package leetcode;
 
 public class Problem28 {
+
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad","sad"));
+    }
+
     public static int strStr(String haystack, String needle) {
         int hLen = haystack.length();
         int nLen = needle.length();
