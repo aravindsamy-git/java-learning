b0943f5080d44b3821a05d84b1e63ac3be081b90
aravindsamy-git <samyaravind53@gmail.com>
2024-09-11 22:13:02 +0530

    added leetcode problem22

--- src/leetcode/Problem22.java
+++ src/leetcode/Problem22.java
@@ -1,4 +1,5 @@
 package leetcode;
+import java.util.*;
 
 public class Problem22 {
     public List<String> generateParenthesis(int n) {
@@ -7,20 +8,14 @@ public class Problem22 {
         return result;
     }
 
-    // Helper method for backtracking
     private void backtrack(List<String> result, String current, int open, int close, int max) {
-        // If the current string has reached the maximum length (2 * n), add it to the result
         if (current.length() == max * 2) {
             result.add(current);
             return;
         }
-
-        // Add an open parenthesis if the number of open parentheses is less than n
         if (open < max) {
             backtrack(result, current + "(", open + 1, close, max);
         }
-
-        // Add a close parenthesis if the number of close parentheses is less than open
         if (close < open) {
             backtrack(result, current + ")", open, close + 1, max);
         }
