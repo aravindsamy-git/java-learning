bf50b7e29891c10bf52c86529cc3db4dbd66d645
aravindsamy-git <samyaravind53@gmail.com>
2024-09-12 22:03:05 +0530

    added subset problem using recursion

--- src/recursionImp/SubSet.java
+++ src/recursionImp/SubSet.java
@@ -1,6 +1,7 @@
 package recursionImp;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SubSet {
@@ -16,14 +17,27 @@ public class SubSet {
     static List<List<Integer>> subset(int[] arr){
         List<List<Integer>> outer  = new ArrayList<>();
 
+        Arrays.sort(arr);
         outer.add(new ArrayList<>());
 
-        for(int num: arr){
+        int start = 0;
+        int end = 0;
+
+        for(int i = 0; i < arr.length; i++){
+
+            start = 0;
+
+            if (i>0 && arr[i] == arr[i-1]){
+                start = end + 1;
+            }
+
+            end = outer.size() - 1;
+
             int size = outer.size();
 
-            for (int i = 0; i < size; i++) {
-                List<Integer> internal = new ArrayList<>(outer.get(i));
-                internal.add(num);
+            for (int j = start; j < size; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
                 outer.add(internal);
             }
         }
