f5a3f79c8ed554b7b206771b61c727ea40c8af9f
aravindsamy-git <samyaravind53@gmail.com>
2024-09-13 21:33:12 +0530

    added leetcode problem35

--- src/leetcode/Problem35.java
+++ src/leetcode/Problem35.java
@@ -10,7 +10,6 @@ public class Problem35 {
     public static int searchInsert(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
 
@@ -22,8 +21,7 @@ public class Problem35 {
                 right = mid - 1;
             }
         }
-
-        // If target is not found, left will be the insert position
         return left;
     }
+
 }
