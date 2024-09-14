69aacc2312d713058b4d90b56376f565785dd787
aravindsamy-git <samyaravind53@gmail.com>
2024-09-14 17:45:53 +0530

    added maze problem  using recursion

--- src/recursionImp/MazeProblem.java
+++ src/recursionImp/MazeProblem.java
@@ -1,18 +1,49 @@
 package recursionImp;
 
+import java.util.ArrayList;
+
 public class MazeProblem {
     public static void main(String[] args) {
-        System.out.println(maze(3,3));
+        System.out.println(pathCount(3,3));
+        path("",3,3);
+        System.out.println(pathReturn("",3,3));
     }
 
-    static int maze(int r , int c){
+    static int pathCount(int r , int c){
         if(r == 1 || c == 1){
             return 1;
         }
-
-        int left = maze(r-1,c);
-        int right = maze(r,c-1);
-
+        int left = pathCount(r-1,c);
+        int right = pathCount(r,c-1);
         return left + right;
+    }
+
+    static void path(String p , int r , int c){
+        if(r == 1 && c == 1){
+            System.out.println(p);
+            return;
+        }
+        if(r > 1){
+            path(p + 'D', r - 1, c);
+        }
+        if(c > 1){
+            path(p + 'R', r, c - 1);
+        }
+    }
+
+    static ArrayList<String> pathReturn(String p , int r , int c){
+        if(r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r > 1){
+            list.addAll(pathReturn(p + 'D', r - 1, c));
+        }
+        if(c > 1){
+            list.addAll(pathReturn(p + 'R', r, c - 1));
+        }
+        return list;
     }
 }
