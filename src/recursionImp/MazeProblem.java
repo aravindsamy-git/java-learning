080c9c7a1bd672c0855068e5e09614a5259199d5
aravindsamy-git <samyaravind53@gmail.com>
2024-09-14 17:50:45 +0530

    added maze problem  using recursion

--- src/recursionImp/MazeProblem.java
+++ src/recursionImp/MazeProblem.java
@@ -7,6 +7,7 @@ public class MazeProblem {
         System.out.println(pathCount(3,3));
         path("",3,3);
         System.out.println(pathReturn("",3,3));
+        System.out.println(pathReturnDiagonal("",3,3));
     }
 
     static int pathCount(int r , int c){
@@ -44,6 +45,27 @@ public class MazeProblem {
         if(c > 1){
             list.addAll(pathReturn(p + 'R', r, c - 1));
         }
+        return list;
+    }
+
+    static ArrayList<String> pathReturnDiagonal(String p , int r , int c){
+        if(r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+
+        if(r > 1 && c > 1){
+            list.addAll(pathReturnDiagonal(p + 'D',r - 1, c - 1));
+        }
+        if(r > 1){
+            list.addAll(pathReturnDiagonal(p + 'V', r - 1, c));
+        }
+        if(c > 1){
+            list.addAll(pathReturnDiagonal(p + 'H', r, c - 1));
+        }
+
         return list;
     }
 }
