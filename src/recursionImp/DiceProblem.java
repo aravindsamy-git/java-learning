907a25f9bfb1dec6849b879b297bb7daeacbd861
aravindsamy-git <samyaravind53@gmail.com>
2024-09-14 15:54:56 +0530

    added dice problem using recursion

--- src/recursionImp/DiceProblem.java
+++ src/recursionImp/DiceProblem.java
@@ -1,9 +1,12 @@
 package recursionImp;
 
+import java.util.ArrayList;
+
 public class DiceProblem {
 
     public static void main(String[] args) {
         dice("",4);
+        System.out.println(diceReturn("",4));
     }
 
     static void dice(String p, int target){
@@ -15,5 +18,22 @@ public class DiceProblem {
         for (int i = 1; i <=6 && i <= target ; i++) {
             dice(p + i,target - i);
         }
+    }
+
+    static ArrayList<String> diceReturn(String p , int target){
+
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i = 1; i <=6 && i <= target ; i++) {
+            list.addAll(diceReturn(p + i, target - i));
+        }
+
+        return list;
     }
 }
