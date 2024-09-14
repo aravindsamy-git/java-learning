a59e7ab2f190b5581b8af2abaf17b9dc7dfa153d
aravindsamy-git <samyaravind53@gmail.com>
2024-09-14 15:01:52 +0530

    added leetcode problem17

--- src/leetcode/Problem17.java
+++ src/leetcode/Problem17.java
@@ -1,43 +1,4 @@
 package leetcode;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
 
 public class Problem17 {
-    private static final String[] KEYPAD = {
-            "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
-    };
-
-    public static void main(String[] args) {
-        String digits = "79";
-        List<String> result = letterCombinations(digits);
-        System.out.println(result);
-    }
-
-    public static List<String> letterCombinations(String digits) {
-        List<String> combinations = new ArrayList<>();
-        if (digits == null || digits.length() == 0) {
-            return combinations;
-        }
-
-        Queue<String> queue = new LinkedList<>();
-        queue.offer("");
-
-        for (char digit : digits.toCharArray()) {
-            String letters = KEYPAD[digit - '0'];
-
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                String combination = queue.poll();
-
-                for (char letter : letters.toCharArray()) {
-                    queue.offer(combination + letter);
-                }
-            }
-        }
-
-        combinations.addAll(queue);
-        return combinations;
-    }
 }
