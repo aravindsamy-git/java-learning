684462252b2961abfe8079eb5c84eebc2f3892fa
aravindsamy-git <samyaravind53@gmail.com>
2024-09-14 14:43:29 +0530

    added string permutation using recursion

--- src/recursionImp/StrPermutation.java
+++ src/recursionImp/StrPermutation.java
@@ -1,8 +1,11 @@
 package recursionImp;
 
+import java.util.ArrayList;
+
 public class StrPermutation {
     public static void main(String[] args) {
         permutation("","abc");
+        System.out.println(permutationList("","abc"));
     }
 
     static void permutation(String p , String up){
@@ -19,5 +22,26 @@ public class StrPermutation {
             String s = p.substring(i,p.length());
             permutation(f + ch + s,up.substring(1));
         }
+    }
+
+    static ArrayList<String> permutationList(String p , String up){
+
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+            ans.addAll(permutationList(f + ch + s,up.substring(1)));
+        }
+
+        return ans;
     }
 }
