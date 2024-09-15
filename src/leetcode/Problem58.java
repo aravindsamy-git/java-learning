e86025946d03188b6ed6c8934f5ac2ddbc9e6695
aravindsamy-git <samyaravind53@gmail.com>
2024-09-15 21:20:10 +0530

    added leetcode problem58

--- src/leetcode/Problem58.java
+++ src/leetcode/Problem58.java
@@ -1,5 +1,21 @@
 package leetcode;
 
 public class Problem58 {
+    public static int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
 
+        // Skip any trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Now count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
 }
