0375d58c858b19443efe8de191ec036cb6f25b56
aravindsamy-git <samyaravind53@gmail.com>
2024-09-15 21:20:25 +0530

    added leetcode problem58

--- src/leetcode/Problem58.java
+++ src/leetcode/Problem58.java
@@ -1,6 +1,11 @@
 package leetcode;
 
 public class Problem58 {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord("   fly me   to   the moon  "));
+    }
+
     public static int lengthOfLastWord(String s) {
         int length = 0;
         int i = s.length() - 1;
