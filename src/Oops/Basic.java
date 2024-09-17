089fc55f0f7f848c5705de7846a8b717567fb851
aravindsamy-git <samyaravind53@gmail.com>
2024-09-17 18:53:51 +0530

    Initialized and Started Oops

--- src/Oops/Basic.java
+++ src/Oops/Basic.java
@@ -1,4 +1,26 @@
 package Oops;
 
+import java.util.Arrays;
+
 public class Basic {
+
+    public static void main(String[] args) {
+        Student student1 = new Student();
+        student1.name = "Aravind";
+        student1.rno = 123456789;
+        student1.marks = 98;
+
+        System.out.println(student1.getName());
+    }
+
+}
+
+class Student{
+    int rno;
+    String name;
+    float marks;
+
+    public String getName() {
+        return name;
+    }
 }
