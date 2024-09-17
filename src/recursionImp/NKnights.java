0cb172e7a30c06023d2b23163af6e84389cfebc9
aravindsamy-git <samyaravind53@gmail.com>
2024-09-17 11:34:21 +0530

    added N-Knights problem  using recursion

--- src/recursionImp/NKnights.java
+++ src/recursionImp/NKnights.java
@@ -1,4 +1,80 @@
 package recursionImp;
 
 public class NKnights {
+
+    static void Knight(boolean[][] board, int row, int col , int knights){
+
+        if (knights == 0){
+            display(board);
+            return;
+        }
+
+        if(row == board.length - 1 && col == board.length){
+            return;
+        }
+
+        if(col == board.length) {
+            Knight(board,row + 1,0,knights);
+            return;
+        }
+
+        if (isSafe(board,row,col)){
+            board[row][col] = true;
+            Knight(board,row,col + 1,knights - 1);
+            board[row][col] = false;
+        }
+
+        Knight(board,row,col+1,knights);
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col){
+        if (isvalid(board,row - 2,col - 1)){
+            if (board[row - 2][col - 1]) {
+                return false;
+            }
+        }
+
+        if (isvalid(board,row - 1,col - 2)){
+            if (board[row - 1][col - 2]) {
+                return false;
+            }
+        }
+
+        if (isvalid(board,row - 2,col + 1)){
+            if (board[row - 2][col + 1]) {
+                return false;
+            }
+        }
+
+        if (isvalid(board,row - 1,col + 2)){
+            if (board[row - 1][col + 2]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static boolean isvalid(boolean[][] board, int row, int col){
+        if(row >= 0 && row < board.length && col >=0 && col < board.length){
+            return true;
+        }
+
+        return false;
+    }
+
+    private static void display(boolean[][] board){
+        for (boolean[] row: board){
+            for (boolean element: row){
+                if(element) {
+                    System.out.print("K");
+                }
+                else {
+                    System.out.print(".");
+                }
+            }
+            System.out.println();
+        }
+    }
+
 }
