5069ef2e547b8e4d800e1f7b55e582ed05829808
aravindsamy-git <samyaravind53@gmail.com>
2024-09-19 23:16:24 +0530

    added leetcode problem66

--- src/leetcode/Problem66.java
+++ src/leetcode/Problem66.java
@@ -1,8 +1,15 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class Problem66 {
 
-    public int[] plusOne(int[] digits) {
+    public static void main(String[] args) {
+        int[] digits = {1,2,3};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+    public static int[] plusOne(int[] digits) {
         for (int i = digits.length - 1; i >= 0; i--) {
             if (digits[i] < 9) {
                 digits[i]++;
