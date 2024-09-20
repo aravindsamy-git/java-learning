02f38cfd10ed2610f4abe3ca35d32de0d721847b
aravindsamy-git <samyaravind53@gmail.com>
2024-09-20 23:37:48 +0530

    added leetcode problem5

--- src/leetcode/Problem5.java
+++ src/leetcode/Problem5.java
@@ -32,17 +32,13 @@ public class Problem5 {
     }
 
     public static boolean checkPalindrom(String s){
-
         for (int i = 0; i < s.length()/2; i++) {
-
             char start = s.charAt(i);
             char end = s.charAt(s.length() - 1 - i);
-
             if(start != end){
                 return false;
             }
         }
-
         return true;
     }
 
