2faed15961758f2c6484d9e3d67e44a396e7b641
aravindsamy-git <samyaravind53@gmail.com>
2024-09-20 20:49:08 +0530

    added leetcode problem214

--- src/leetcode/Problem214.java
+++ src/leetcode/Problem214.java
@@ -1,4 +1,28 @@
 package leetcode;
 
 public class Problem214 {
+
+    public static String shortestPalindrome(String s) {
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        StringBuilder S = new StringBuilder(s);
+
+        while (i < j){
+
+            if (S.charAt(i) == S.charAt(j)){
+                i++;
+                j--;
+                System.out.println("same");
+                continue;
+            }
+
+            S.insert(i,S.charAt(j));
+            i++;
+        }
+
+        return new String(S);
+    }
+
 }
