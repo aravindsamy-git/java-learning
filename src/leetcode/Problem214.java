9e6234df995ae33f5ac445fbf6996fe8c02661da
aravindsamy-git <samyaravind53@gmail.com>
2024-09-20 21:11:50 +0530

    added leetcode problem214

--- src/leetcode/Problem214.java
+++ src/leetcode/Problem214.java
@@ -6,22 +6,42 @@ public class Problem214 {
         System.out.println(shortestPalindrome("abcd"));
     }
 
-    public static String shortestPalindrome(String s) {
-        int i = 0;
-        int j = s.length() - 1;
-        StringBuilder S = new StringBuilder(s);
+    public  static String shortestPalindrome(String s) {
+        StringBuilder rev = new StringBuilder(s).reverse();  // Reverse the string
+        String revStr = rev.toString();
 
-        while (i < j){
-            if (S.charAt(i) == S.charAt(j)){
-                i++;
-                j--;
-                System.out.println("same");
-                continue;
-            }
-            S.insert(i,S.charAt(j));
-            i++;
-        }
-        return new String(S);
+        // Create a string that appends original string and reversed string with a separator
+        String combined = s + "#" + revStr;
+
+        // Find the longest palindromic prefix
+        int[] lps = computeLPS(combined);  // Get longest prefix which is also suffix
+
+        // Characters to add from the reverse string (remaining part)
+        String toAdd = revStr.substring(0, s.length() - lps[lps.length - 1]);
+
+        return toAdd + s;  // Prepend to original string
     }
 
+    private static int[] computeLPS(String str) {
+        int n = str.length();
+        int[] lps = new int[n];
+        int len = 0;
+        int i = 1;
+
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
 }
