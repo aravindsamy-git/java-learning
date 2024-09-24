2bccd88824561346049577743a66ae64d5fa1d7c
aravindsamy-git <samyaravind53@gmail.com>
2024-09-24 21:31:51 +0530

    added leetcode problem15

--- src/leetcode/Problem15.java
+++ src/leetcode/Problem15.java
@@ -1,4 +1,45 @@
 package leetcode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Problem15 {
+    public static void main(String[] args) {
+
+    }
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        // Sort the array
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates for the second and third elements
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
 }
