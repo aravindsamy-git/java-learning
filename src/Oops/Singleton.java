1c07952383c4e9f60d650f0ff84b3fc8328f3191
aravindsamy-git <samyaravind53@gmail.com>
2024-09-24 21:18:41 +0530

    added Singleton in Oops

--- src/Oops/Singleton.java
+++ src/Oops/Singleton.java
@@ -1,4 +1,17 @@
 package Oops;
 
 public class Singleton {
+    private Singleton (){
+
+    }
+
+    private static Singleton instance;
+
+    public static Singleton getInstance() {
+        if(instance == null){
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
 }
