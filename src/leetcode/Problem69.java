3f6f8e097daa17b722d0f2cfbd31bc00cf0ded06
aravindsamy-git <samyaravind53@gmail.com>
2024-09-27 19:56:16 +0530

    added leetcode problem69

--- src/leetcode/Problem69.java
+++ src/leetcode/Problem69.java
@@ -13,7 +13,6 @@ public class Problem69 {
         int left = 1;
         int right = x;
         int result = 0;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (mid <= x / mid) {
@@ -25,5 +24,4 @@ public class Problem69 {
         }
         return result;
     }
-
 }
