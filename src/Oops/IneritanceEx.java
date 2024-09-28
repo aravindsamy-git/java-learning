9682aa3ee4298a44d65c8b1e8e91a4011b4aee29
aravindsamy-git <samyaravind53@gmail.com>
2024-09-28 21:59:08 +0530

    added Inheritance in Oops

--- src/Oops/IneritanceEx.java
+++ src/Oops/IneritanceEx.java
@@ -1,4 +1,48 @@
 package Oops;
 
 public class IneritanceEx {
+    double l;
+    double h;
+    double w;
+
+    IneritanceEx () {
+        this.h = -1;
+        this.l = -1;
+        this.w = -1;
+    }
+
+    IneritanceEx (double side) {
+        this.w = side;
+        this.l = side;
+        this.h = side;
+    }
+
+    IneritanceEx(double l,double h,double w) {
+        this.l = l;
+        this.h = h;
+        this.w = w;
+    }
+
+    IneritanceEx (IneritanceEx old) {
+        this.h = old.h;
+        this.l = old.l;
+        this.w = old.w;
+    }
+
+    public void information() {
+        System.out.println("Runnning the box");
+    }
+}
+
+class BoxWeight extends IneritanceEx{
+    double weight;
+
+    public BoxWeight(){
+        this.weight = -1;
+    }
+
+    public BoxWeight(double l, double h , double w , double weight){
+        super(l,h,w);
+        this.weight = weight;
+    }
 }
---
+++ src/Oops/InheritanceMain.java
@@ -0,0 +1,10 @@
+package Oops;
+
+public class InheritanceMain {
+    public static void main(String[] args) {
+
+        BoxWeight box = new BoxWeight();
+
+        System.out.println(box.l+ " " + box.h + " "+ box.w + " " + box.weight);
+    }
+}
