5473f334f5e46a0573cf354d20a984c0ece8b41a
aravindsamy-git <samyaravind53@gmail.com>
2024-09-29 22:46:33 +0530

    added leetcode problem70

--- src/leetcode/Problem70.java
+++ src/leetcode/Problem70.java
@@ -6,21 +6,16 @@ public class Problem70 {
     }
 
     public static int climbStairs(int target){
-
         if(target == 0){
             return 1;
         }
-
         int count = 0;
-
         if(target >= 2){
             count = count + climbStairs(target - 2);
         }
-
         if (target >= 1){
             count = count + climbStairs(target - 1);
         }
-
         return count;
     }
 }
