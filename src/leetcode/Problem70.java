3098d78c91aa3a64f9933e4fa93b5395ab292eeb
aravindsamy-git <samyaravind53@gmail.com>
2024-09-29 22:46:13 +0530

    added leetcode problem70

--- src/leetcode/Problem70.java
+++ src/leetcode/Problem70.java
@@ -2,10 +2,10 @@ package leetcode;
 
 public class Problem70 {
     public static void main(String[] args) {
-        System.out.println(climbes(1));
+        System.out.println(climbStairs(1));
     }
 
-    public static int climbes(int target){
+    public static int climbStairs(int target){
 
         if(target == 0){
             return 1;
@@ -14,11 +14,11 @@ public class Problem70 {
         int count = 0;
 
         if(target >= 2){
-            count = count + climbes(target - 2);
+            count = count + climbStairs(target - 2);
         }
 
         if (target >= 1){
-            count = count + climbes(target - 1);
+            count = count + climbStairs(target - 1);
         }
 
         return count;
