68ab5b74fd9601fec7d44d1e30c294cb289c43f7
aravindsamy-git <samyaravind53@gmail.com>
2024-09-29 23:30:19 +0530

    added leetcode problem60

--- src/leetcode/Problem60.java
+++ src/leetcode/Problem60.java
@@ -19,12 +19,10 @@ public class Problem60 {
     public static String createNumber(int n) {
         StringBuilder numStr = new StringBuilder();
 
-        // Append numbers from 1 to n to the StringBuilder
         for (int i = 1; i <= n; i++) {
             numStr.append(i);
         }
 
-        // Convert the resulting string to an integer
         return numStr.toString();
     }
 
@@ -52,12 +50,10 @@ public class Problem60 {
     public static ArrayList<Integer> convertAndSort(ArrayList<String> stringList) {
         ArrayList<Integer> numberList = new ArrayList<>();
 
-        // Convert each String to an Integer and add to numberList
         for (String str : stringList) {
             numberList.add(Integer.parseInt(str));
         }
 
-        // Sort the list of integers
         Collections.sort(numberList);
 
         return numberList;
