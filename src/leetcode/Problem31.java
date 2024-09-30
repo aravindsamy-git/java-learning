c9863dc1c1b2f628dfc3e5f2c149619c7283d781
aravindsamy-git <samyaravind53@gmail.com>
2024-09-30 18:09:06 +0530

    added leetcode problem31

--- src/leetcode/Problem31.java
+++ src/leetcode/Problem31.java
@@ -10,26 +10,21 @@ public class Problem31 {
         int n = nums.length;
         int i = n - 2;
 
-        // Step 1: Find the first decreasing element from the right
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
 
-        // Step 2: If such an element is found, find the element to swap with
         if (i >= 0) {
             int j = n - 1;
             while (j >= 0 && nums[j] <= nums[i]) {
                 j--;
             }
-            // Step 3: Swap the elements
             swap(nums, i, j);
         }
 
-        // Step 4: Reverse the sequence after the first decreasing element
         reverse(nums, i + 1, n - 1);
     }
 
-    // Utility function to swap two elements in an array
     private static void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
