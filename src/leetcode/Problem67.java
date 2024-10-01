09bf35fa95e6ef79a46e1b1a21fa613416b46ea9
aravindsamy-git <samyaravind53@gmail.com>
2024-10-01 23:30:09 +0530

    added leetcode problem67

--- src/leetcode/Problem67.java
+++ src/leetcode/Problem67.java
@@ -8,34 +8,28 @@ public class Problem67 {
 
     public static String addBinary(String a, String b) {
         StringBuilder result = new StringBuilder();
-        int i = a.length() - 1;  // Pointer for string a
-        int j = b.length() - 1;  // Pointer for string b
-        int carry = 0;  // Carry initialized to 0
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
 
-        // Process both strings from right to left
         while (i >= 0 || j >= 0 || carry == 1) {
-            int sum = carry;  // Add carry to the sum
+            int sum = carry;
 
-            // If there's a bit in a, add it to the sum
             if (i >= 0) {
                 sum += a.charAt(i) - '0';
                 i--;
             }
 
-            // If there's a bit in b, add it to the sum
             if (j >= 0) {
                 sum += b.charAt(j) - '0';
                 j--;
             }
 
-            // Append the sum % 2 to the result (this is the current bit)
             result.append(sum % 2);
 
-            // Update carry (either 0 or 1)
             carry = sum / 2;
         }
 
-        // The result is in reverse order, so reverse it
         return result.reverse().toString();
     }
 }
