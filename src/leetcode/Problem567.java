355bc0b7d225ba02462db808918bde1c3840a376
aravindsamy-git <samyaravind53@gmail.com>
2024-10-05 22:02:49 +0530

    added leetcode problem567

--- src/leetcode/Problem567.java
+++ src/leetcode/Problem567.java
@@ -14,23 +14,18 @@ public class Problem567 {
 
 
     static ArrayList<String> permutationList(String p , String up){
-
         if(up.isEmpty()){
             ArrayList<String> list = new ArrayList<>();
             list.add(p);
             return list;
         }
-
         char ch = up.charAt(0);
-
         ArrayList<String> ans = new ArrayList<>();
-
         for (int i = 0; i <= p.length(); i++) {
             String f = p.substring(0,i);
             String s = p.substring(i,p.length());
             ans.addAll(permutationList(f + ch + s,up.substring(1)));
         }
-
         return ans;
     }
 
