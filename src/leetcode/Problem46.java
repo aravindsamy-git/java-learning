7b8b1f43c3a86aea2878bec9005d1b3e1a3a47d3
aravindsamy-git <samyaravind53@gmail.com>
2024-10-06 19:50:39 +0530

    added leetcode problem46

--- src/leetcode/Problem46.java
+++ src/leetcode/Problem46.java
@@ -5,16 +5,7 @@ import java.util.ArrayList;
 public class Problem46 {
 
     public static void main(String[] args) {
-        int current = 1;
 
-        // Create a mutable list
-        List<Integer> list = new ArrayList<>(List.of(2, 3, 4));
-
-        for (int i = 0; i < list.size(); i++) {
-            list.add(i, current); // Add 'current' at position 'i'
-            System.out.println(list);
-            list.remove(i); // Remove 'current' from position 'i' to restore original state
-        }
     }
 
     public static List<List<Integer>> permuatationList (List<Integer> p , List<Integer> up ) {
