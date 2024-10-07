bd785a771ce519b3262ec9d7d5487d6e383302c7
aravindsamy-git <samyaravind53@gmail.com>
2024-10-07 21:25:09 +0530

    added leetcode problem2696

--- src/leetcode/Problem2696.java
+++ src/leetcode/Problem2696.java
@@ -1,4 +1,18 @@
 package leetcode;
 
 public class Problem2696 {
+
+
+    public static int minLength(String s) {
+
+        String current = s;
+
+        while (current.contains("AB") || current.contains("CD")) {
+            current = current.replace("AB", "");  // Remove first substring
+            current = current.replace("CD", "");  // Remove second substring
+        }
+
+        return current.length();
+
+    }
 }
