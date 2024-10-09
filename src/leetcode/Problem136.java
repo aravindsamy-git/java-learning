fa82aff12e5e468f9a0179f5d549c565c68c1518
aravindsamy-git <samyaravind53@gmail.com>
2024-10-09 21:45:15 +0530

    added leetcode problem136

--- src/leetcode/Problem136.java
+++ src/leetcode/Problem136.java
@@ -10,7 +10,7 @@ public class Problem136 {
     public static int singleNumber(int[] nums) {
         int singleNumber = 0;
         for (int num : nums) {
-            singleNumber ^= num;  // XOR all numbers
+            singleNumber ^= num;
         }
         return singleNumber;
     }
