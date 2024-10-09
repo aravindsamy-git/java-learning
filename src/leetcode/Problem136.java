cf875b5962ce5686ad2b0230574e4dc3a1f21f29
aravindsamy-git <samyaravind53@gmail.com>
2024-10-09 21:44:30 +0530

    added leetcode problem136

--- src/leetcode/Problem136.java
+++ src/leetcode/Problem136.java
@@ -2,6 +2,11 @@ package leetcode;
 
 public class Problem136 {
 
+    public static void main(String[] args) {
+        int[] nums = {4,2,1,1,2};
+        System.out.println(singleNumber(nums));
+    }
+
     public static int singleNumber(int[] nums) {
         int singleNumber = 0;
         for (int num : nums) {
